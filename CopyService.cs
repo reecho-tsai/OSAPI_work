8dff4313f84dbc2c9690a847ebeab9b38b531784
RP Cai <8058474+vtility@user.noreply.gitee.com>
2022-10-09 13:39:22 +0800

    ADD: file copy and test of folder browse

---
+++ CopyService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using NetDimension.NanUI.Resource.Data;
+using NetDimension.NanUI.Browser.ResourceHandler;
+
+using OSAPI;
+using OSAPI.Struct;
+
+public class CopyService : DataService
+{
+    private CopyProgressResult CopyProgressHandler(long total, long transferred, long streamSize, long StreamByteTrans, uint dwStreamNumber, CopyProgressCallbackReason reason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData)
+    {
+        return CopyProgressResult.PROGRESS_CONTINUE;
+    }
+    public ResourceResponse DirCopy(ResourceRequest request)
+    {
+        [DllImport("shell32.dll")]
+        static extern IntPtr SHBrowseForFolder(ref BROWSEINFO lpbi);
+        IntPtr pidl = IntPtr.Zero;
+        BROWSEINFO bi = new BROWSEINFO();
+        bi.lpszTitle = "hello";
+
+        pidl =  SHBrowseForFolder(ref bi);
+
+        return Text(pidl.ToString());
+    }
+
+    public ResourceResponse FileCopy(ResourceRequest request)
+    {
+        [DllImport("Comdlg32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool GetOpenFileName(ref OpenFileName ofn);
+
+        var ofn = new OpenFileName();
+        ofn.lStructSize = Marshal.SizeOf(ofn);
+        ofn.lpstrFilter = "All files(*.*)\0\0";
+        ofn.lpstrFile = new string(new char[256]);
+        ofn.nMaxFile = ofn.lpstrFile.Length;
+        ofn.lpstrFileTitle = new string(new char[64]);
+        ofn.nMaxFileTitle = ofn.lpstrFileTitle.Length;
+        ofn.lpstrTitle = "Select File";
+        if (GetOpenFileName(ref ofn))
+        {
+            [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            static extern bool CopyFileEx(string lpExistingFileName, string lpNewFileName,
+              CopyProgressRoutine lpProgressRoutine, IntPtr lpData, ref Int32 pbCancel,
+              CopyFileFlags dwCopyFlags);
+
+            int pbCancel;
+
+            if (CopyFileEx(ofn.lpstrFile, "G:\\1.html", new CopyProgressRoutine(this.CopyProgressHandler), IntPtr.Zero, ref pbCancel, CopyFileFlags.COPY_FILE_RESTARTABLE))
+            // TODO: Assign dst as the removeable disk
+            {
+                return Text("Succeed");
+            }
+        }
+        return Text("Failed");
+    }
+        
+}
---
+++ Struct/CopyStruct.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+using OSAPI;
+
+namespace OSAPI.Struct
+{
+    enum CopyProgressResult : uint
+    {
+        PROGRESS_CONTINUE = 0,
+        PROGRESS_CANCEL = 1,
+        PROGRESS_STOP = 2,
+        PROGRESS_QUIET = 3
+    }
+
+    enum CopyProgressCallbackReason : uint
+    {
+        CALLBACK_CHUNK_FINISHED = 0x00000000,
+        CALLBACK_STREAM_SWITCH = 0x00000001
+    }
+
+    delegate CopyProgressResult CopyProgressRoutine(
+    long TotalFileSize,
+    long TotalBytesTransferred,
+    long StreamSize,
+    long StreamBytesTransferred,
+    uint dwStreamNumber,
+    CopyProgressCallbackReason dwCallbackReason,
+    IntPtr hSourceFile,
+    IntPtr hDestinationFile,
+    IntPtr lpData);
+
+    [Flags]
+    enum CopyFileFlags : uint
+    {
+        COPY_FILE_FAIL_IF_EXISTS = 0x00000001,
+        COPY_FILE_RESTARTABLE = 0x00000002,
+        COPY_FILE_OPEN_SOURCE_FOR_WRITE = 0x00000004,
+        COPY_FILE_ALLOW_DECRYPTED_DESTINATION = 0x00000008,
+        COPY_FILE_COPY_SYMLINK = 0x00000800, //NT 6.0+
+        COPY_FILE_NO_BUFFERING = 0x00001000 //NT 6.0+
+    }
+}
